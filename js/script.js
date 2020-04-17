1beb3767aa8b6e8e5568df93b2d04ca4112ec583
Ermolenko-Sergey <ermolenko.sergey.petrovich@gmail.com>
2020-04-17 18:10:09 +0300

    Last test bootstrap.

--- js/script.js
+++ js/script.js
@@ -20,3 +20,20 @@ $(document).ready(function(){
         }
     });
 });
+
+$(document).ready(function() {
+    var button = $('#button-up');
+    $(window).scroll (function () {
+        if ($(this).scrollTop () > 300) {
+            button.fadeIn();
+        } else {
+            button.fadeOut();
+        }
+    });
+    button.on('click', function(){
+        $('body, html').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
+});
